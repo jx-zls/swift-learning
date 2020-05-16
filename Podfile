0208b1e3acf4518b843b4943e9d2b1ff585cf22a
贾金勋 <1195380833@qq.com>
2020-05-16 18:00:56 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'Swift-Project' do
+
+pod 'SwiftyJSON', '~> 4.2.0'
+pod 'Alamofire', '~> 4.7.0'
+pod 'SnapKit', '~> 4.2.0'
+pod 'Kingfisher', '~> 4.7.0'
+
+end
---
+++ Swift-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-Project
+//
+//  Created by 贾金勋 on 2020/5/16.
+//  Copyright © 2020 Luke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
